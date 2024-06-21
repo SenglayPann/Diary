ba3035f5f021910753fb29c6ddb319daee7777b2
Pann Senglay <senglaypann@gmail.com>
2024-06-21 14:45:19 +0700

    Add: added responsivenes of header

---
+++ components/header.js
@@ -0,0 +1,32 @@
+class Header extends HTMLElement {
+    constructor() {
+        super()
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+        <nav id="navbar">
+            <div id="site-name">Diary Aplication</div>
+            <div id="dark-mode-toggler">
+                <div id="toggler-handle">
+                    <img src="./assets/icons/sun.svg" alt="">
+                    <img class="is-hidden" src="./assets/icons/moon.svg" alt="">
+                </div>
+            </div>
+        </nav>
+        `;
+
+        const themeTogglerHandle = document.getElementById('toggler-handle');
+        const themeTogglerHandleContainer = document.getElementById('dark-mode-toggler');
+
+        themeTogglerHandleContainer.addEventListener('click', () => {
+            themeTogglerHandle.classList.toggle('is-bg-black');
+            themeTogglerHandle.classList.toggle('clicked-dark-mode'); // move the dark mode handle toggler
+            themeTogglerHandle.children[0].classList.toggle('is-hidden'); 
+            themeTogglerHandle.children[1].classList.toggle('is-hidden');
+
+        });
+    }
+}
+
+customElements.define('custom-header', Header)
