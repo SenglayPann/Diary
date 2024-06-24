15e98f2d4dce4e91bc8f650dc2060a091885e414
Pann Senglay <senglaypann@gmail.com>
2024-06-24 18:52:00 +0700

    Add: addedd animattion to the landing page

--- components/landing-page.js
+++ components/landing-page.js
@@ -9,14 +9,14 @@ class LandingPage extends HTMLElement {
             <div id="landing-page-bg">
                 <div id="landing-page-sheet">
                     <div id="landing-page-content">
-                        <div id="landing-page-block1">
+                        <div id="landing-page-block1" class="slide-right">
                             <div>Welcome to Your Personal Diary</div>
                             <img src="./assets/interactive_graphics/book-with-scetch.png" alt="">
                         </div>
 
-                        <div id="landing-page-block2">
+                        <div id="landing-page-block2" class="fade-in">
                             <div>this Capture <span>MOMENTS</span>, Share <span>MEMORIES</span>, and Reflect on Your <span>JOURNEY </span>!</div>
-                            <button><a href="./pages/diary-list.html">GET STARTED</a></button>
+                            <button>GET STARTED</button>
                         </div>
                     </div>
                 </div>
